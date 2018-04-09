5875a780494624bfaf491b7c9cc7fbcfb12b9441
Miquel <miquelgalianallorca@gmail.com>
2018-04-09 20:16:18 +0200

    Example connection

--- Chat/Client/main.cpp
+++ Chat/Client/main.cpp
@@ -1,9 +1,72 @@
-// CLIENT
-
+#include <stdio.h>
 #include <iostream>
+#include <string>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
-int main()
+using namespace std;
+
+#define SERVER_PORT "12345"
+#define BUF_SIZE 4096
+
+// CLIENT
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	WSADATA wsaData;
+	int wsaerr;
+	SOCKET sockfd;
+	char buf[BUF_SIZE];
+	memset(&buf, 0, sizeof(buf));
+	if (argc < 2) return -1;
+	wsaerr = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaerr != 0)
+	{
+		printf("Could not initialize Winsock.\n");
+		return -1;
+	}
+	struct addrinfo hints;// hints de la direccion buscada
+	struct addrinfo* servInfo;// contendra la lista de direccciones encontradas
+	struct addrinfo* srvaddr;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	// Busca las addrinfo del host al que nos queremos conectar
+	int error = getaddrinfo(argv[1], SERVER_PORT, &hints, &servInfo);
+
+	if (error != 0)
+	{
+		printf("getaddrinfo failed");
+		WSACleanup();
+		return -1;
+	}
+	for (srvaddr = servInfo; (srvaddr != NULL); srvaddr = srvaddr->ai_next)
+	{
+		sockfd = socket(srvaddr->ai_family, srvaddr->ai_socktype, srvaddr->ai_protocol);
+		if (sockfd != INVALID_SOCKET) {
+			int iResult = connect(sockfd, srvaddr->ai_addr, (int)srvaddr->ai_addrlen);
+			if (iResult == SOCKET_ERROR) {
+				closesocket(sockfd);
+				sockfd = INVALID_SOCKET;
+			}
+			else
+				break;
+		}
+	}
+	freeaddrinfo(servInfo);
+	if (sockfd != INVALID_SOCKET)
+	{
+		gets_s(buf, _countof(buf));
+		int length = strlen(buf) + 1;
+
+		int numBytesSend, totalSend = 0;
+		do
+		{
+			numBytesSend = send(sockfd, buf, length - totalSend, 0);
+			totalSend += numBytesSend;
+		} while (totalSend < length);
+		closesocket(sockfd);
+	}
+	WSACleanup();
 	return 0;
 }
--- Chat/Server/main.cpp
+++ Chat/Server/main.cpp
@@ -1,9 +1,72 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+
+#define SERVER_PORT 12345
+#define BUF_SIZE 4096
+#define QUEUE_SIZE 10
+
 // SERVER
-
-#include <iostream>
-
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	WSADATA wsaData;
+	int wsaerr;
+	SOCKET sockId, socketCliId;
+	int on = 1;
+	struct sockaddr_in addr;
+	struct sockaddr_in client;
+	char buf[BUF_SIZE];
+	wsaerr = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaerr != 0) {
+		printf("Could not initialize Winsock.\n");
+		return -1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET; // Aquí NO se usa htons.
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(SERVER_PORT);
+
+	sockId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sockId < 0) {
+		printf("Error creating socket\n");
+		WSACleanup();
+		return -1;
+	}
+	setsockopt(sockId, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+	if (bind(sockId, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+		printf("Error binding port\n");
+		WSACleanup();
+		return -1;
+	}
+	if (listen(sockId, QUEUE_SIZE) < 0) {
+		printf("listen failed\n");
+		WSACleanup();
+		return -1;
+	}
+	while (1) {
+		socklen_t sock_len = sizeof(client);
+		memset(&client, 0, sizeof(client));
+		socketCliId = accept(sockId, (struct sockaddr *)&client, &sock_len);
+		if (socketCliId == INVALID_SOCKET)
+			printf("Can't accept client:%d\n", WSAGetLastError());
+		else {
+			int rec = 0;
+			int totalRecv = 0;
+			do {
+				rec = recv(socketCliId, buf + totalRecv, BUF_SIZE, 0);
+				totalRecv += rec;
+				printf("rec:%d | totalRecv: %d\n", rec, totalRecv);
+			} while (buf[totalRecv - 1] != '\0');
+			char ipClient[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, &client.sin_addr, ipClient, sizeof(ipClient));
+			printf("Client:%s Port:%d\n", ipClient, ntohs(client.sin_port));
+			printf("Message Length: %d\n", totalRecv);
+			printf("Message: %s\n", buf);
+			closesocket(socketCliId);
+		}
+	}
+	closesocket(sockId);
+	WSACleanup();
 	return 0;
 }
